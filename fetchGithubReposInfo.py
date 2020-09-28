fe60d58e00c4c836b2dc72c46d514b7d6d8cf294
Anshul Kapoor <anshulkapoor018@gmail.com>
2020-09-28 16:11:20 -0400

    Code refactoring

--- fetchGithubReposInfo.py
+++ fetchGithubReposInfo.py
@@ -12,39 +12,39 @@ import json
 
 def fetchRepos(user_id):
     """ Fetch user's repositories and commits"""
+    repo_storage = []
     repo_api = "https://api.github.com/users/"
-    commit_api = "https://api.github.com/repos/"
     fetch_url = repo_api + user_id + '/repos'
-    output = []
+
     get_url = requests.get(fetch_url)
     repo_list = get_url.json()
 
     try:
         for line in repo_list:
             repo = line.get('name')
-            output.append(repo)
+            repo_storage.append(repo)
     except (TypeError, KeyError, IndexError, AttributeError):
         raise ValueError('Not able to Fetch repositories from Entered User ID')
 
-    return output
+    return repo_storage
 
 def number_of_commits(user_name, repo_name):
-    """ to get the number of commits in a repository """
+    """ Takes input as user's id and repo name and returns number of commits in that repo """
     get_url = requests.get('https://api.github.com/repos/{}/{}/commits'.format(user_name, repo_name))
-    commits = get_url.json()
+    num_commits = get_url.json()
 
-    if commits == 0:
-        print('The respective repositories has no commits!')
+    if num_commits == 0:
+        print('The respective repository has no commits!')
 
-    return len(commits)
+    return len(num_commits)
 
 def main():
     """ Get user's Github ID as input """
     user_name = input("Please enter the GitHub user ID: ")
 
     for repo in fetchRepos(user_name):
-        num = number_of_commits(user_name, repo)
-        print(f"Repo: {repo} Number of commits: {num}")
+        num_of_commits = number_of_commits(user_name, repo)
+        print(f"Repo: {repo} Number of commits: {num_of_commits}")
 
 
 if __name__ == '__main__':
