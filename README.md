7b0d6980ac4be57a3429697bd0a9a1f7fd79b557
Anshul Kapoor <anshulkapoor018@gmail.com>
2020-10-07 16:44:54 -0400

    Added mockedRequests

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # GithubApi_Testing_567
 ### Fetch Github Repositories Info
 
-[![build status of master](https://travis-ci.org/anshulkapoor018/GithubApi_Testing_567.svg?branch=master)](https://travis-ci.org/anshulkapoor018/GithubApi_Testing_567)
+[![build status of HW05a_Mocking'](https://travis-ci.org/anshulkapoor018/GithubApi_Testing_567.svg?branch=HW05a_Mocking')](https://travis-ci.org/anshulkapoor018/GithubApi_Testing_567)
--- fetchGithubReposInfo_Test.py
+++ fetchGithubReposInfo_Test.py
@@ -6,12 +6,16 @@ The primary goal of this file is to Test our fetchGithubReposInfo Function
 """
 __author__ = "Anshul Kapoor"
 
+from unittest import mock
 import unittest
 from fetchGithubReposInfo import fetchRepos, number_of_commits
 
 class TestFetchGithubRepos(unittest.TestCase):
     """ Test cases to check the results from API results"""
-    def test_getHub_repo(self):
+
+    @mock.patch('fetchGithubReposInfo.number_of_commits')
+    def test_getHub_repo(self, mocked_request):
+        mocked_request.return_value = 30
         with self.assertRaises(ValueError):
             fetchRepos("Restaurant-Ratings-Project")
         self.assertEqual(number_of_commits("anshulkapoor018", "Restaurant-Ratings-Project"), 30)
